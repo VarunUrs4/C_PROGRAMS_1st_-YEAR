f5b5208b33d26ef9fe6d3c53a083c09b0af4447c
VarunUrs4 <84217264+VarunUrs4@users.noreply.github.com>
2021-07-14 22:50:40 +0530

    Update Temperature conversion.c

--- Temperature conversion.c
+++ Temperature conversion.c
@@ -4,7 +4,7 @@ int main()
     float C,F;
     printf("Enter the temperature in Fahrenheit scale\n");
     scanf("%f",&F);
-    C=(F-32)*5/9;
+    C=(F-32)*5.0/9.0;
     printf("The temperature in degree Celsius is %f", C);
     return 0;
 }
